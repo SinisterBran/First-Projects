a2e2070cb69bba2094467cc9c48a10816321ee2d
Sinister01 <99212451+SinisterBran@users.noreply.github.com>
2023-04-03 09:06:13 -0700

    Create Calculator.py
    Simple calculator, used in console.

---
+++ Calculator.py
@@ -0,0 +1,50 @@
+#Very simple calculator, first create functions for either a multiplying variable adding variable ETC.
+#After prompt user to choose their choice of equation and number than add
+
+
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    return x / y
+
+# Main function
+print("Please select operation -\n" \
+        "1. Add\n" \
+        "2. Subtract\n" \
+        "3. Multiply\n" \
+        "4. Divide\n")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice (1/2/3/4): ")
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
